05b57fd9e1e06a47ec00a62a35b9efafe969ddae
ViniciusDinizz <viniciusdiniz.ti/2gmail.com>
2023-03-03 14:38:16 -0300

    Add project files.

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Exercicio4
+{
+    private static void Main(string[] args)
+    {
+        //Leia um número qualquer e imprima se ele é primo ou não
+        int cont = 0, primo = 0;
+
+        Console.WriteLine("Digite um número para saber se é primo: ");
+        double num = double.Parse(Console.ReadLine());
+
+        cont = int.Parse(num.ToString());
+
+        while (cont > 0)
+        {
+            if(num%cont == 0)
+            {
+                primo++;
+            }
+            cont--;
+        }
+        if(primo == 2)
+        {
+            Console.WriteLine($"Número {num} é primo.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {num} não é primo.");
+        }
+
+    }
+}
